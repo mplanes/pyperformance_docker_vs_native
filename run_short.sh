b995fca5a37b71efb5c7f351a16e357c5c2fae85
Martin Planes <martin.planes@gmail.com>
2019-09-06 12:13:24 +0200

    [docker] remove second docker image

--- run_short.sh
+++ run_short.sh
@@ -2,7 +2,6 @@
 
 native_results=/tmp/native_$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 12).json
 docker_results=/tmp/docker_$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 12).json
-docker_results2=/tmp/docker_$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 12).json
 ### native benchmark
 type -P python3.7 &>/dev/null || { echo "python3.7 binary not found, installing" ; sudo apt install python3.7; }
 python3.7 -m pip install virtualenv
@@ -18,8 +17,5 @@ mkdir /tmp/venv_docker_debian
 docker build . -f Dockerfile.py3perfdebian -t pyperfdebian
 docker run -v /tmp:/tmp --privileged -v venv:/venv pyperfdebian pyperformance run --venv /venv --python=python3 -b nqueens -r --affinity 5,6 -o $docker_results
 
-docker build . -f Dockerfile.py3perfdebian2 -t pyperfdebian2
-docker run -v /tmp:/tmp --privileged -v venv:/venv  pyperfdebian2 pyperformance run --venv /venv --python=python3 -b nqueens -r --affinity 5,6 -o $docker_results2
-
 # display results
-python3 -m pyperf compare_to $native_results $docker_results $docker_results2 --table
+python3 -m pyperf compare_to $native_results $docker_results --table
